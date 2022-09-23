3546b503bee0f1b5390b3dfcf077c433a6ad2807
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2022-09-22 21:15:28 -0300

    last updates part 2

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HomeComponent } from './home/home.component'
-import { RoutingModule } from './routing.module';
-import { InputComponent } from './input/input.component'
+import { RoutingModule } from './routing.module'
 
 @NgModule({
-  declarations: [HomeComponent, InputComponent],
+  declarations: [HomeComponent],
   imports: [CommonModule, RoutingModule],
 })
 export class PagesModule {}
